70b75bd9e5c2766fa775e6af6763ddaf2667b8c7
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-05-04 17:25:56 +0530

    Logout Function Added

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -6,7 +7,27 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Chats')),
+      appBar: AppBar(
+        title: const Text('Chats'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.exit_to_app_rounded,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            onPressed: () async {
+              try {
+                FirebaseAuth.instance.signOut();
+              } on FirebaseAuthException {
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Error signing out!')),
+                );
+              }
+            },
+          ),
+        ],
+      ),
       body: const Center(child: Text("Hello!")),
     );
   }
